18986dbcd0fa054cfda8bf8c5a918bc1b974c3ea
dfnabiullin <nabiullindanis333@gmail.com>
2025-11-28 21:19:43 +0500

    chore: Unify dependency versions

--- task-service/build.gradle.kts
+++ task-service/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 plugins {
     java
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.flywaydb.flyway") version "11.15.0"
 }
